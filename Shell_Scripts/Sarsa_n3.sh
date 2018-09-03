a6f8424f1fb10ced4439967952a0e0f6e189f4e0
jfernando <jfhernan@ualberta.ca>
2018-09-03 09:10:05 -0600

    added 3 more scripts for running experiments

---
+++ Shell_Scripts/Sarsa_n3.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo Enter the number of agents you want to train:
+read number_of_agents
+echo Enter the number of the first agent:
+read first_agent_number
+last_agent_number=$(($first_agent_number + $number_of_agents - 1))
+
+export PYTHONPATH=.
+for (( i=$first_agent_number; i <= $last_agent_number; i++ ))
+do
+    echo "Training Agent $i"
+    python3.6 /home/jfernando/PycharmProjects/RL_Experiments/Experiments/Deep_QSigma_MC/dqsigman_mc.py -episodes 500 \
+    -n 3 -sigma 1 -beta 1 -quiet -dump_agent -target_epsilon 0.1 -compute_bprobabilities -name Sarsa_n3/agent_$i
+done
