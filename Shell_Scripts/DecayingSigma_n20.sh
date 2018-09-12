4b5ee16b70e474fde833e9d5adde75b7113384ca
jfernando <jfhernan@ualberta.ca>
2018-09-11 23:10:14 -0600

    Added script: decaying sigma and hand picked sigma

---
+++ Shell_Scripts/DecayingSigma_n20.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo Enter the number of agents you want to train:
+read number_of_agents
+echo Enter the number of the first agent:
+read first_agent_number
+last_agent_number=$(($first_agent_number + $number_of_agents - 1))
+
+export PYTHONPATH=.
+for (( i=$first_agent_number; i <= $last_agent_number; i++ ))
+do
+    echo "Training Agent $i"
+    python3 ./Experiments/Deep_QSigma_MC/dqsigman_mc.py -episodes 500 -n 20 -sigma 1 -beta 0.95 -store_sigma \
+    -compute_bprobabilities -quiet -dump_agent -target_epsilon 0.1 -name DecayingSigma_n20/agent_$i
+done
