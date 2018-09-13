2d4176ff28fcb5842a59ac260984c45e0f197344
JFernando4 <jfhernan@ualberta.ca>
2018-09-13 06:21:25 -0600

    more ds scripts

---
+++ Shell_Scripts/DecayingSigma_Tnet_Ufreq500_n3.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo Enter the number of agents you want to train:
+read number_of_agents
+echo Enter the number of the first agent:
+read first_agent_number
+last_agent_number=$(($first_agent_number + $number_of_agents - 1))
+
+export PYTHONPATH=.
+for (( i=$first_agent_number; i <= $last_agent_number; i++ ))
+do
+    echo "Training Agent $i"
+    python3 ./Experiments/Deep_QSigma_MC/dqsigman_mc.py -episodes 500 -n 3 -sigma 1 -beta 0.95 -store_sigma \
+    -compute_bprobabilities -quiet -dump_agent -target_epsilon 0.1 -tnetwork_update_freq 500 \
+    -name DecayingSigma_Tnet_Ufreq500_n3/agent_$i
+done
