9cd311349456321b258a4721ba7db833b5755044
JFernando4 <jfhernan@ualberta.ca>
2018-10-04 21:30:55 -0600

    added sarsa on policy script

--- Shell_Scripts/nStep_Retrace_Tnet_Ufreq2000_n20.sh
+++ Shell_Scripts/nStep_Retrace_Tnet_Ufreq2000_n20.sh
@@ -11,5 +11,5 @@ for (( i=$first_agent_number; i <= $last_agent_number; i++ ))
 do
     echo "Training Agent $i"
     python3 ./Experiments/Deep_nStep_Retrace/deep_nstep_retrace.py -episodes 500 -n 20 -quiet -dump_agent \
-    -tnetwork_update_freq 2000 name Retrace_Tnet_Ufreq2000_n20/agent_$i
+    -tnetwork_update_freq 2000 -name Retrace_Tnet_Ufreq2000_n20/agent_$i
 done
